e747cfaea31f1d6ba6c672b09cf807e3a341231d
HakPyun <kjiz0503@gmail.com>
2025-05-11 14:04:36 +0900

    서프프로젝트 초기 설정

---
+++ src/main/java/com/example/SSStudy/SsStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.SSStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SSStudy/SsStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SSStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
